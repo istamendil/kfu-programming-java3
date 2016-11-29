0de1ae68d754d5a2eb2b748eea465bbea9aeb223
Alexander Ferenets <ist.kazan@gmail.com>
2016-11-29 12:29:43 +0300

    Simple Http Server removed from Maven + updated to real Simple Http Server

--- simpleHttpServerSockets/ru/kpfu/simple_http_server/Server.java
+++ simpleHttpServerSockets/ru/kpfu/simple_http_server/Server.java
@@ -1,28 +1,36 @@
-package ru.kpfu.core;
+package ru.kpfu.simple_http_server;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 /**
- *
+ * Simple HTTP Server.
+ * 
  * @author Alexander Ferenets (Istamendil) <ist.kazan@gmail.com>
  */
 public class Server {
 
   private static ServerSocket serverSocket;
   private static Socket socket;
+  /**
+   * Port to listen.
+   */
   private static final int PORT = 8080;
+  /**
+   * Default response
+   */
+  private static final String MESSAGE = "Hello world!";
 
   public Server() {
     try {
+      // Create server listening PORT
       serverSocket = new ServerSocket(Server.PORT);
-      while (true) {
-        // Get Request
-        socket = serverSocket.accept();
+      // Get new connection
+      while ((socket = serverSocket.accept()) != null) {
+        // REad request
         BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
         while (true) {
           String s = br.readLine();
@@ -31,10 +39,16 @@ public class Server {
           }
           System.out.println(s);
         }
-        // Create and send answer
-        OutputStream os = socket.getOutputStream();
-        os.write("Hi!".getBytes());
-        os.flush();
+        System.out.println("---");
+        // Create and send response
+        PrintWriter pr = new PrintWriter(socket.getOutputStream());
+        pr.println("HTTP/1.1 200 OK");
+        pr.println("Content-type:text/plain;Charset:UTF-8");
+        pr.println("Content-length:" + MESSAGE.length());
+        pr.println();
+        pr.println(MESSAGE);
+        pr.flush();
+        socket.close();
       }
     } catch (Exception ex) {
       System.err.println("Error: " + ex.getMessage());
